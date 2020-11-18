ff14b3fa90e003dcac7e87010a230d6c1abe959c
bloodywing <bloodywing@tastyespresso.de>
2020-11-18 15:16:23 +0100

    Fix for windows build, which doesn't require a .so dynamic library

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import argparse
+import platform
 from ctypes import cdll
 from gettext import gettext as _
 from moongame.game import Game
 
-# This is a hack to help pyinstaller detecting the sdl2 library
-cdll.LoadLibrary('libSDL2_image-2.0.so.0')
+if platform.system() == 'Linux':
+    # This is a hack to help pyinstaller detecting the sdl2 library
+    cdll.LoadLibrary('libSDL2_image-2.0.so.0')
 
 
 if __name__ == '__main__':
@@ -12,5 +14,6 @@ if __name__ == '__main__':
     parse.add_argument('-f', '--fullscreen', action='store_true', default=False, help=_('Start the game in fullscreen'))
     parse.add_argument('-s', '--scale', default=2, type=int, help=_('Set scaling for the game'))
     parse.add_argument('-d', '--debug', default=False, action='store_true', help=_('Enable debugging'))
+    parse.add_argument('--scene', default=None, type=str)
     args = parse.parse_args()
     Game(args)
